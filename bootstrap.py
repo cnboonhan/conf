f91e0b27e0124177295e32983cf403e72d99de1b
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-05-21 15:15:24 +0800

    Create bootstrap.py

---
+++ bootstrap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import subprocess
+import lsb_release
+import sys
+import os
+from typing import List
+
+def _install_dependencies(deps: List[str]) -> None:
+    match lsb_release.get_distro_information()['ID']:
+        case 'Ubuntu':
+            p = subprocess.run('sudo apt install -y'.split() + deps)
+        case _:
+            raise Exception("Unrecognized OS. Terminating..")
+            sys.exit(1)
+
+def _get_core_repository(path: str = os.path.expanduser('~')):
+    subprocess.run(f"git clone https://github.com/cnboonhan/conf {path}/.conf".split())
+
+if __name__ == '__main__':
+    _install_dependencies(['git'])
+    _get_core_repository()

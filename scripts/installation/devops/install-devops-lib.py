1a74ebd7bfe92740a4ab800e774cf9c35f71a6e9
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-05-22 13:07:38 +0800

    add session-manager plugin

--- scripts/installation/devops/install-devops-lib.py
+++ scripts/installation/devops/install-devops-lib.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from common import _in_virtualenv, _install_pip_dependencies, _install_dependencies
 import pathlib
+import subprocess
 import os
 
 assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
@@ -9,3 +10,11 @@ home_path = pathlib.Path(os.path.expanduser('~'))
 
 _install_pip_dependencies(dir_path / 'requirements.txt')
 _install_dependencies(['python3-tk'])
+subprocess.run(
+    "curl https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb \
+            -o /tmp/session-manager-plugin.deb".split()
+).check_returncode()
+
+subprocess.run(
+    "sudo dpkg -i /tmp/session-manager-plugin.deb".split()
+).check_returncode()

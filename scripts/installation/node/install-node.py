5bcabcfde88ebfdc05028b88a44a17e633e47a7e
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-05-22 10:59:14 +0800

    Update install-node.py

--- scripts/installation/node/install-node.py
+++ scripts/installation/node/install-node.py
@@ -9,7 +9,7 @@ assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
 dir_path = pathlib.Path(__file__).parent.resolve()
 home_path = pathlib.Path(os.path.expanduser('~'))
 
-shutil.move(home_path / '.bashrc', '/tmp/.bashrc')
+shutil.copy(home_path / '.bashrc', '/tmp/.bashrc')
 
 subprocess.run(
     'wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh'

60cf74cc2612fcc41544135f3dc7ceffc8b726ca
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-05-22 09:53:40 +0800

    add code-server

--- scripts/installation/vscode-server/install-vscode-server.py
+++ scripts/installation/vscode-server/install-vscode-server.py
@@ -3,18 +3,14 @@ from common import _in_virtualenv
 import pathlib
 import os
 import subprocess
-import shutil
 
 assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
 dir_path = pathlib.Path(__file__).parent.resolve()
 home_path = pathlib.Path(os.path.expanduser('~'))
 
-shutil.move(home_path / '.bashrc', '/tmp/.bashrc')
-
 subprocess.run(
-    'wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh'
+    'curl -fsSL https://code-server.dev/install.sh'
     .split(),
     stdout=open('/tmp/install.sh', 'w')).check_returncode()
 
 subprocess.run('bash /tmp/install.sh'.split()).check_returncode()
-shutil.move('/tmp/.bashrc', home_path / '.bashrc')

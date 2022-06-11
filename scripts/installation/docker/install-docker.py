7299a47abdaa558e101482296fe4e06d4ff690ad
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-06-11 15:02:31 +0800

    fix username getting for docker

--- scripts/installation/docker/install-docker.py
+++ scripts/installation/docker/install-docker.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-from common import _in_virtualenv, _run_command
+from common import _in_virtualenv, _run_command, _get_username
 import pathlib
 import os
 
 assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
 dir_path = pathlib.Path(__file__).parent.resolve()
 home_path = pathlib.Path(os.path.expanduser('~'))
-username = os.getlogin()
+username = _get_username()
 docker_url = 'https://get.docker.com'
 docker_tmp_path = '/tmp/get-docker.sh'
 

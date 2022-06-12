bf5ee8e6cd32917a85742dd9b1dab5df31fc64ff
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-06-12 23:51:54 +0800

    Update install-shell.py

--- scripts/installation/shell/install-shell.py
+++ scripts/installation/shell/install-shell.py
@@ -15,7 +15,6 @@ _install_dependencies(['netcat', 'xclip'])
 #    _run_command("sudo dpkg -i /tmp/gc.deb")
 
 os.makedirs(home_path / '.ssh', exist_ok=True)
-os.makedirs(home_path / '.decrypt/.ssh', exist_ok=True)
 _create_conf_symlink(dir_path / '.bashrc', home_path / '.bashrc')
 _create_conf_symlink(dir_path / 'ssh_config', home_path / '.ssh/config')
 _create_conf_symlink(dir_path / '.ignore', home_path / '.ignore')

8864c36bfd9586907ce49dac67bcf00027cc681b
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-06-12 23:50:13 +0800

    Update install-shell.py

--- scripts/installation/shell/install-shell.py
+++ scripts/installation/shell/install-shell.py
@@ -8,18 +8,11 @@ assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
 dir_path = pathlib.Path(__file__).parent.resolve()
 home_path = pathlib.Path(os.path.expanduser('~'))
 
-_install_dependencies(['netcat', 'gocryptfs', 'xclip'])
+_install_dependencies(['netcat', 'xclip'])
 
 #if not shutil.which('google-chrome'):
 #    _run_command("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/gc.deb") 
 #    _run_command("sudo dpkg -i /tmp/gc.deb")
-
-if not os.path.isdir(home_path / '.encrypt'):
-    print("Creating encrypted directory as it does not exist")
-    _create_encrypt_folder(home_path / '.encrypt')
-
-os.makedirs(home_path / '.decrypt', exist_ok=True)
-_decrypt_folders(home_path / '.encrypt', home_path / '.decrypt')
 
 os.makedirs(home_path / '.ssh', exist_ok=True)
 os.makedirs(home_path / '.decrypt/.ssh', exist_ok=True)

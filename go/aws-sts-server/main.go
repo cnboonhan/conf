3eb1251d5309b49d1e764bb0570e23180ea037fd
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-06-18 20:14:01 +0800

    rudimdentary implementation of sts getter

---
+++ go/aws-sts-server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+func sts(w http.ResponseWriter, req *http.Request) {
+	id_json, _ := exec.Command("aws", "sts", "get-caller-identity").CombinedOutput()
+
+	var id_map map[string]interface{}
+	json.Unmarshal([]byte(id_json), &id_map)
+	arn := id_map["Arn"].(string)
+
+  assume_role_json, _ := exec.Command("aws", "sts", "assume-role", "--role-arn", arn, "--role-session-name", "assume-role").CombinedOutput()
+  fmt.Fprintf(w, "%s", assume_role_json)
+}
+
+func main() {
+	http.HandleFunc("/sts", sts)
+	http.ListenAndServe(":8090", nil)
+}

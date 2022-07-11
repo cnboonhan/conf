aac104176e43c553b4508d869155b9f237d943b0
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-07-11 14:01:18 +0800

    test broken build

--- scripts/installation/webdriver/install-chrome.py
+++ scripts/installation/webdriver/install-chrome.py
@@ -8,6 +8,5 @@ assert _in_virtualenv(), "Please source [path to repo]/.venv/bin/activate."
 dir_path = pathlib.Path(__file__).parent.resolve()
 home_path = pathlib.Path(os.path.expanduser('~'))
 
-if not shutil.which('google-chrome'):
-   _run_command("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/gc.deb") 
-   _run_command("sudo apt install /tmp/gc.deb -y")
+_run_command("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/gc.deb") 
+_run_command("sudo apt install /tmp/gc.deb -y")

